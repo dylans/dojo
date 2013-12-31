6b05cd114dea68c96c8b74f1446773cd40f42873
dylans <dylan@dojotoolkit.org>
2013-12-30 17:09:01 -0700

    more work converting router tests from DOH to Intern

--- tests-intern/unit/router.js
+++ tests-intern/unit/router.js
@@ -1,16 +1,15 @@
 define([
 	'intern!object',
 	'intern/chai!assert',
-	'dojo/_base/array',
-	'dojo/hash',
+	'intern/dojo/_base/array',
+	'intern/dojo/hash',
 	'dojo/router/RouterBase'
 ], function (registerSuite, assert, arrayUtil, hash, RouterBase) {
 
 	// This test uses RouterBase so that we can test a few different
 	// behaviors of the router which require re-initializing a new router
-	var count = 0,
-		router,
-		handle, foo;
+	var count = 0, a, b, testObject,
+		router, handle, foo;
 
 	// Simple helper to make tearDown simpler
 	function removeAll(handles) {
@@ -117,7 +116,7 @@ define([
 			'regex': function () {
 				router.startup('');
 				handle = registerRoute(/^\/bar$/);
-				router.go("/bar");
+				router.go('/bar');
 				assert.strictEqual(count, 1, 'Count should have been 1, was ' + count);
 			}
 		},
@@ -157,7 +156,104 @@ define([
 			}
 		},
 		'route arguments' : {
+			beforeEach: function () {
+				hash('');
+				router = new RouterBase();
+			},
+			afterEach: function () {
+				handle.remove();
+				if (router._started) {
+					router.destroy();
+				}				
+			},
+			'string route': function () {
+				a = b = null;
+				handle = router.register('/stringtest/:applied/:arg', function(event, applied, arg){
+					a = applied;
+					b = arg;
+				});
+				router.go('/stringtest/extra/args');
 
+				assert.strictEqual(a, 'extra', 'a should have been extra, was ' + a);
+				assert.strictEqual(b, 'args', 'b should have been args, was ' + b);
+			},
+			'regex route': function () {
+				a = b = null;
+				handle = router.register(/\/regextest\/(\w+)\/(\w+)/, function(event, applied, arg){
+					a = applied;
+					b = arg;
+				});
+				router.go('/regextest/extra/args');
+
+				assert.strictEqual(a, 'extra', 'a should have been extra, was ' + a);
+				assert.strictEqual(b, 'args', 'b should have been args, was ' + b);
+			},
+			'string route, long with placeholders': function () {
+				testObject = null;
+				handle = router.register('/path/:to/:some/:long/*thing', function(event, applied, arg){
+					testObject = event.params;
+				});
+				router.go('/path/to/some/long/thing/this/is/in/splat');
+
+				assert.isObject(testObject, 'testObject should have been an object')
+				assert.strictEqual(testObject.to, 'to', 'testObject.to should have been to, was ' + testObject.to);
+				assert.strictEqual(testObject.some, 'some', 'testObject.some should have been some, was ' + testObject.some);
+				assert.strictEqual(testObject['long'], 'long', 'testObject.long should have been long, was ' + testObject['long']);
+				assert.strictEqual(testObject.thing, 'thing/this/is/in/splat', 'testObject.thing should have been thing/this/is/in/splat, was ' + testObject.thing);
+			},
+			'string route, numerical with placeholders': function () {
+				testObject = null;
+				handle = router.register('/path/:to/:some/:long/*thing', function(event, applied, arg){
+					testObject = event.params;
+				});
+				router.go('/path/1/2/3/4/5/6');
+
+				assert.isObject(testObject, 'testObject should have been an object')
+				assert.strictEqual(testObject.to, '1', 'testObject.to should have been 1, was ' + testObject.to);
+				assert.strictEqual(testObject.some, '2', 'testObject.some should have been 2, was ' + testObject.some);
+				assert.strictEqual(testObject['long'], '3', 'testObject.long should have been 3, was ' + testObject['long']);
+				assert.strictEqual(testObject.thing, '4/5/6', 'testObject.thing should have been 4/5/6, was ' + testObject.thing);
+			}
+		},
+		'route arguments, capture groups' : {
+			beforeEach: function () {
+				testObject = null;
+				hash('');
+				router = new RouterBase();
+				handle = router.register(/^\/path\/(\w+)\/(\d+)$/, function(event){
+					testObject = event.params;
+				});
+			},
+			afterEach: function () {
+				handle.remove();
+				if (router._started) {
+					router.destroy();
+				}				
+			},
+			'full match': function () {
+				router.go('/path/abcdef/1234');
+
+				assert.isArray(testObject, 'testObject should have been an array')
+				assert.strictEqual(testObject[0], 'abcdef', 'testObject[0] should have been abcdef, was ' + testObject[0]);
+				assert.strictEqual(testObject[1], '1234', 'testObject[1] should have been 1234, was ' + testObject[1]);
+			},
+			'no match': function () {
+				router.go('/path/abc/def');
+
+				assert.ok(!testObject, 'testObject should have been null')
+			},
+			'no match alternate': function () {
+				router.go('/path/abc123/def');
+
+				assert.ok(!testObject, 'testObject should have been null')
+			},
+			'full match, alternate': function () {
+				router.go('/path/abc123/456');
+
+				assert.isArray(testObject, 'testObject should have been an array')
+				assert.strictEqual(testObject[0], 'abc123', 'testObject[0] should have been abc123, was ' + testObject[0]);
+				assert.strictEqual(testObject[1], '456', 'testObject[1] should have been 456, was ' + testObject[1]);
+			}
 		}
 	});
 });
